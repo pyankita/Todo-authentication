afa012a998877ef2ab47f9b341c74e85f2e6de90
pyankita <pyankitaghartimagar@gmail.com>
2025-04-28 20:38:04 +0545

    templates

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("",views.TaskList.as_view(),name="task"),
+    path("detail/<int:pk>/",views.TaskDetailView.as_view(),name="detail"),
+    path("create/",views.TaskCreateView.as_view(),name="create"),
+    path("update/<int:pk>/",views.TaskUpdateView.as_view(),name="update"),
+    path("delete/<int:pk>/",views.TaskDeleteView.as_view(),name="delete")
 
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,33 @@
-from asyncio import Task
+from django.urls import reverse_lazy
+from .models import Task
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView,CreateView,UpdateView,DeleteView
+
 
 class TaskList(ListView):
     model=Task
-    
+    template_name="todo_app/task_list.html"
+    context_object_name="tasks"
+
+class TaskDetailView(DetailView):
+    model=Task
+    template_name="todo_app/task_detail.html"
+    context_object_name="task"
+
+class TaskCreateView(CreateView):
+    model=Task
+    fields="__all__" #display all field if only want one ==> ["title "]
+    template_name="todo_app/task_create.html"
+    success_url=reverse_lazy("task") #task is from the url redirect to the url 
+
+class TaskUpdateView(UpdateView):
+    model=Task
+    fields="__all__" #display all field if only want one ==> ["title "]
+    template_name="todo_app/task_create.html"
+    success_url=reverse_lazy("task")
+
+class TaskDeleteView(DeleteView):
+    model=Task
+    context_object_name="tasks"
+    success_url="task"
+    template_name="todo_app/task_delete.html"
